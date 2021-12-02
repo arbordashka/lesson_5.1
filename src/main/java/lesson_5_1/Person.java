88a4e2ef3f59ee6b616dff55d1f964981c7dce33
arbordashka <dasha_galalu@mail.ru>
2021-12-02 19:09:12 +0300

    Initial commit

---
+++ src/main/java/lesson_5_1/Person.java
@@ -0,0 +1,21 @@
+package lesson_5_1;
+
+public class Person {
+
+        public String name;
+        public String position;
+        public String email;
+        public String phone;
+        public int salary;
+        public int age;
+
+        public Person (String name, String position, String email, String phone, int salary, int age) {
+                this.name = name;
+                this.position = position;
+                this.email = email;
+                this.phone = phone;
+                this.salary = salary;
+                this.age = age;
+        }
+
+    }
